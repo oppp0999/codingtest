28fa5e910a7ab58f5182630a6d5f4efd398c3481
oppp0999 <oppp0999@naver.com>
2022-12-18 01:25:50 +0900

    20221218_1

--- c++/BAEKJOON/c++ 기초 101-150/4458-첫글자 대문자/4458.cpp
+++ c++/BAEKJOON/c++ 기초 101-150/4458-첫글자 대문자/4458.cpp
@@ -1,1 +1,22 @@
-ignore()함수는 입력한 이후 입력버퍼를 지워주는 역할을 합니다.
+//ignore()함수는 입력한 이후 입력버퍼를 지워주는 역할을 합니다.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int c=0;
+    string a;
+
+    cin >> c;
+    cin.ignore();
+
+    while(c--){
+        getline(cin,a);
+        if(a[0]>= 'a' && a[0]<= 'z'){
+            a[0] = a[0] - 32;}
+        cout << a << endl;
+        //std::endl 버퍼를 비우는 코드
+    }
+
+}
