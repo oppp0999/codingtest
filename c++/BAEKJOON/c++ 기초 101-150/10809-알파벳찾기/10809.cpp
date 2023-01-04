c3b6e50f9be54f5270e92f9a4f7d08153af66a4d
oppp0999 <oppp0999@naver.com>
2023-01-04 18:24:41 +0900

    20230104_1

--- c++/BAEKJOON/c++ 기초 101-150/10809-알파벳찾기/10809.cpp
+++ c++/BAEKJOON/c++ 기초 101-150/10809-알파벳찾기/10809.cpp
@@ -6,6 +6,6 @@ int main() {
     string alphabet = "abcdefghijklmnopqrstuvwxyz";
     cin >> s;
     for(int i = 0; i < alphabet.length(); i++)
-        cout << (int)s.find(alphabet[i]) << " ";
+        cout << (int)s.find(alphabet[i]) << " ";//find 찾으면 해당 위치를 반환 없으면 -1을 반환하는 find 함수
     return 0;
 }
